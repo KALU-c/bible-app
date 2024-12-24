2c20810f6cdc12232fe45f94eaa436dd2c701100
Endekalu Zemenu <endekaluzemenu2134@gmail.com>
2024-12-24 08:46:56 -0800

    feat: update chapter navigation buttons position and enhance reader styling

--- src/pages/reader.tsx
+++ src/pages/reader.tsx
@@ -16,17 +16,18 @@ const Reader = () => {
 
   return (
     <div className="mx-96 py-4">
+      <h1 className="text-center text-base mb-8 font-semibold">GENESIS 50</h1>
       {book.map((verse, index) => (
         <>
           {(verse.type === "paragraph text" || verse.type === "line text") && (
             <span
               key={index}
-              className={`mb-1 cursor-pointer ${isFocused.verse === verse.verseNumber && "border-b border-dotted border-b-black"}`}
+              className={`mb-1 cursor-pointer ${isFocused.verse === verse.verseNumber && "border-b border-dotted border-b-black dark:border-b-white"}`}
               onClick={() => setIsFocused({ verse: verse.verseNumber || 0, isFocused: true })}
             >
               <sup
                 className={`
-                  text-xs text-red-500 mr-1 
+                  text-xs text-blue-500 mr-1 
                   ${(verse.type === "line text" && verse.value === " ") && "hidden"}
                   ${(verse.sectionNumber !== 1 && "hidden")}
                 `}
---
+++ src/providers/book-provider.tsx
@@ -0,0 +1,9 @@
+const BookProvider = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default BookProvider

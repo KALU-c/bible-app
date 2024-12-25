ede53b4d04c0d3cc8f9d419b6aafd2b0e323a38d
Endekalu Zemenu <endekaluzemenu2134@gmail.com>
2024-12-24 18:20:56 -0800

    feat: enhance reader component with verse highlighting and sharing options

--- src/pages/reader.tsx
+++ src/pages/reader.tsx
@@ -1,18 +1,27 @@
 import { getBookByChapter } from "@/assets/book/old"
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Separator } from "@/components/ui/separator"
 import { useSidebar } from "@/components/ui/sidebar"
+import { CircleSlash2, Copy, Forward } from "lucide-react"
 import { useState } from "react"
 
 type VerseFocusType = {
-  verse: number,
+  verse: number | undefined,
   isFocused: boolean
+}
+
+type verseHighlightColor = {
+  verse: number | undefined
+  backgroundColor: string
+  textColor?: string
 }
 
 const Reader = () => {
   const { open } = useSidebar();
-  const [isFocused, setIsFocused] = useState<VerseFocusType>({
-    verse: 0,
-    isFocused: false
-  });
+  const [isVerseFocused, setIsVerseFocused] = useState<VerseFocusType[]>([]);
+  const [highlightColor, setHighlightColor] = useState<verseHighlightColor[]>([]);
 
   const book = getBookByChapter("genesis", 50);
 
@@ -25,22 +34,83 @@ const Reader = () => {
       {book.map((verse, index) => (
         <>
           {(verse.type === "paragraph text" || verse.type === "line text") && (
-            <span
-              key={index}
-              className={`mb-1 text-[17px] cursor-pointer ${isFocused.verse === verse.verseNumber && "border-b border-dotted border-b-black dark:border-b-white"}`}
-              onClick={() => setIsFocused({ verse: verse.verseNumber || 0, isFocused: true })}
-            >
-              <sup
-                className={`
+            <Popover>
+              <PopoverTrigger asChild>
+                <span
+                  key={index}
+                  style={highlightColor.find(item => item.verse === verse.verseNumber) && {
+                    color: highlightColor.find(item => item.verse === verse.verseNumber)?.textColor || "black"
+                  }}
+                  className={`
+                    mb-1 text-[17px] cursor-pointer p-1 
+                    bg-${highlightColor?.find(item => item.verse === verse.verseNumber)?.backgroundColor || "bg-none"}
+                    ${isVerseFocused.find(item => item.verse === verse.verseNumber)
+                      ? `border-b border-dotted border-b-black dark:border-b-white rounded-sm`
+                      : "border-none"}
+                  `}
+                  onClick={() => {
+                    isVerseFocused.find(item => item.verse === verse.verseNumber)
+                      ? setIsVerseFocused(isVerseFocused.filter(item => item.verse !== verse.verseNumber))
+                      : setIsVerseFocused([...isVerseFocused, { verse: verse.verseNumber, isFocused: true }])
+                  }}
+                >
+                  <sup
+                    className={`
                   text-xs text-blue-500 mr-1 
                   ${(verse.type === "line text" && verse.value === " ") && "hidden"}
                   ${(verse.sectionNumber !== 1 && "hidden")}
                 `}
-              >
-                {verse.verseNumber}
-              </sup>
-              {verse.value}
-            </span>
+                  >
+                    {verse.verseNumber}
+                  </sup>
+                  {verse.value}
+                </span>
+              </PopoverTrigger>
+              <PopoverContent className="w-52 p-2 flex flex-col gap-1">
+                <div className="flex flex-col gap-2">
+                  <Label className="text-muted-foreground text-xs text-center">Highlight</Label>
+                  <div className="flex flex-row justify-between">
+                    <span
+                      className="w-[30px] h-[30px] rounded-full cursor-pointer flex items-center justify-center"
+                      onClick={() => setHighlightColor([...highlightColor, { verse: verse.verseNumber, backgroundColor: "green-300" }])}
+                    >
+                      <CircleSlash2 />
+                    </span>
+                    <span
+                      className="w-[30px] h-[30px] bg-green-300 rounded-full cursor-pointer"
+                      onClick={() => setHighlightColor([...highlightColor, { verse: verse.verseNumber, backgroundColor: "green-300" }])}
+                    ></span>
+                    <span
+                      className="w-[30px] h-[30px] bg-blue-300 rounded-full cursor-pointer"
+                      onClick={() => setHighlightColor([...highlightColor, { verse: verse.verseNumber, backgroundColor: "blue-300" }])}
+                    ></span>
+                    <span
+                      className="w-[30px] h-[30px] bg-red-300 rounded-full cursor-pointer"
+                      onClick={() => setHighlightColor([...highlightColor, { verse: verse.verseNumber, backgroundColor: "red-300" }])}
+                    ></span>
+                    <span
+                      className="w-[30px] h-[30px] bg-yellow-300 rounded-full cursor-pointer"
+                      onClick={() => setHighlightColor([...highlightColor, { verse: verse.verseNumber, backgroundColor: "yellow-300" }])}
+                    ></span>
+                  </div>
+                </div>
+                <Separator />
+                <div className="flex flex-row justify-between gap-2">
+                  <Button variant="secondary" className="text-xs">
+                    <Forward />
+                    Share
+                  </Button>
+                  <Button
+                    variant="secondary"
+                    className="text-xs w-full"
+                    onClick={() => navigator.clipboard.writeText(`${verse.verseNumber} - ${verse.value}`)}
+                  >
+                    <Copy />
+                    Copy
+                  </Button>
+                </div>
+              </PopoverContent>
+            </Popover>
           )}
 
           {verse.type === "paragraph end" && (

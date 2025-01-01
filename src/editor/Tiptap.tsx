9e7675ca63d781ad75bf0ac7ecbb7357a98c1b46
K A L U <endekaluzemenu2134@gmail.com>
2025-01-01 11:03:57 +0300

    feat: add highlight and horizontal rule extensions to Tiptap editor and implement bubble menu buttons for text formatting

--- src/editor/Tiptap.tsx
+++ src/editor/Tiptap.tsx
@@ -1,16 +1,23 @@
 import { EditorProvider, FloatingMenu, BubbleMenu } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
+import Highlight from "@tiptap/extension-highlight"
+import BubbleMenuButtons from "./bubble-menu-buttons"
 
-const extensions = [StarterKit.configure({
-  heading: {
-    levels: [1, 2, 3]
-  }
-})]
+const extensions = [
+  StarterKit.configure({
+    heading: {
+      levels: [1, 2, 3]
+    }
+  }),
+  Highlight.configure({
+    multicolor: true
+  })
+]
 
 const editorProps = {
   attributes: {
     class: 'prose prose-sm sm:prose-base lg:prose-base xl:prose-lg m-5 focus:outline-none dark:prose-invert prose-headings:text-indigo-500 prose-slate w-full min-h-screen min-w-full',
-  },
+  }
 }
 
 const Tiptap = () => {
@@ -23,7 +30,7 @@ const Tiptap = () => {
         This is floating menu
       </FloatingMenu>
       <BubbleMenu editor={null}>
-        This is bubble menu
+        <BubbleMenuButtons />
       </BubbleMenu>
     </EditorProvider>
   )
---
+++ src/editor/bubble-menu-buttons.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button"
+import { useCurrentEditor } from "@tiptap/react"
+import { Bold, Highlighter } from "lucide-react"
+
+const BubbleMenuButtons = () => {
+  const { editor } = useCurrentEditor()
+
+  return (
+    <div
+      className="bg-zinc-50 dark:bg-zinc-800 rounded-md outline outline-zinc-100 dark:outline-zinc-700 flex flex-row"
+    >
+      <Button
+        variant={editor?.isActive("bold") ? "default" : "ghost"}
+        className="w-7 h-7"
+        onClick={() => editor?.chain().focus().toggleBold().run()}
+      >
+        <Bold />
+      </Button>
+      <Button
+        variant={editor?.isActive("highlight") ? "default" : "ghost"}
+        className="w-7 h-7"
+        onClick={() => editor?.commands.toggleHighlight()}
+      >
+        <Highlighter />
+      </Button>
+    </div>
+  )
+}
+export default BubbleMenuButtons

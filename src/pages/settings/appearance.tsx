8166048ea939834176df1d89295650da78e8d932
K A L U <endekaluzemenu2134@gmail.com>
2025-01-02 11:57:22 +0300

    feat: add theme settings with dark mode toggle and theme selection options

--- src/pages/settings/appearance.tsx
+++ src/pages/settings/appearance.tsx
@@ -1,17 +1,38 @@
 import { Switch } from "@/components/ui/switch"
 import { Fullscreen } from "lucide-react"
+import Theme from "./theme"
+import { useBookSetting } from "@/providers/book-provider"
+import { useTheme } from "@/providers/theme-provider"
 
 const Appearance = () => {
+  const { fontFamily } = useBookSetting()
+  const { setTheme } = useTheme()
+
   return (
-    <div>
+    <div className={`${fontFamily === "Source Serif Pro" ? "font-source-serif" : "font-inter"} flex flex-col space-y-4`}>
       <div className="flex items-center space-x-4 rounded-md border p-4">
         <Fullscreen size={20} />
         <div className="flex-1 space-y-1">
-          <p className="text-sm font-medium leading-none">Fullscreen Mode</p>
+          <p className="text-sm font-semibold leading-none">Fullscreen Mode</p>
           <p className="text-xs text-muted-foreground">Switch to enable or disable fullscreen mode.</p>
         </div>
         <Switch />
       </div>
+      <div className="flex items-center space-x-4 rounded-md border p-4">
+        <Fullscreen size={20} />
+        <div className="flex-1 space-y-1">
+          <p className="text-sm font-semibold leading-none">
+            Dark Mode
+          </p>
+          <p className="text-xs text-muted-foreground">Switch to enable or disable dark mode.</p>
+        </div>
+        <Switch
+          onCheckedChange={(checked: boolean) => {
+            setTheme(checked ? "dark" : "light")
+          }}
+        />
+      </div>
+      <Theme />
     </div>
   )
 }

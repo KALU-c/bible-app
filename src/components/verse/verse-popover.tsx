2c61265b669d4193a00351dc7ad962fb4089378e
K A L U <endekaluzemenu2134@gmail.com>
2025-01-04 07:52:07 +0300

    feat: enhance VersePopover with highlighting options and dropdown menu for color selection

--- src/components/verse/verse-popover.tsx
+++ src/components/verse/verse-popover.tsx
@@ -4,12 +4,14 @@ import {
   PopoverTrigger,
   PopoverContent
 } from "../ui/popover"
-import { X } from "lucide-react"
-import { Label } from "../ui/label"
+import { Bold, Bookmark, ChevronDown, Highlighter, Italic, X } from "lucide-react"
 import { Separator } from "../ui/separator"
 import { HighlightedVersesType, LocalStorageBookObject } from "@/types/book-type"
 import { BookItemType } from "@/assets/book/formatted-json"
 import { CopyToClipBoardButton } from "@/global/copy-to-clipbord-button"
+import { Button } from "../ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
+import { useState } from "react"
 
 type VersePopoverType = {
   book: LocalStorageBookObject
@@ -33,27 +35,35 @@ const VersePopover = ({
   children
 }: VersePopoverType) => {
 
-  function handleHighlightVerse(highlightBackgroundColor: string) {
-    if (isVerseFocused) {
-      if (book2) {
-        // TODO - overwrite the backgroundColor is the verse already exist
-        const newHighlights: HighlightedVersesType[] = isVerseFocused.map(verse => ({
-          reference: { book: book.book2.name, chapter: book.book2.chapter, verse: verse.verse! },
-          background_color: highlightBackgroundColor
-        }));
+  const [currentHighlightColor, setHighlightColor] = useState("#fde047")
 
-        // TODO - saves the last highlighted verse not the present
-        // TODO - can't change already highlighted color
-        setBook({
-          ...book,
-          book2: {
-            ...book.book2,
-            highlightedVerses: [
-              ...book.book2.highlightedVerses,
-              ...newHighlights
-            ]
-          }
-        });
+  function handleHighlightVerse(highlightBackgroundColor: string) {
+    setHighlightColor(highlightBackgroundColor)
+    if (isVerseFocused.length > 0) {
+      if (book2) {
+        const verseAlreadyHighlighted = book.book2.highlightedVerses.find(item => item.reference.verse === verse.verseNumber);
+
+        if (verseAlreadyHighlighted) {
+          verseAlreadyHighlighted.background_color = highlightBackgroundColor;
+        } else {
+          const newHighlights: HighlightedVersesType[] = isVerseFocused.map(verse => ({
+            reference: { book: book.book2.name, chapter: book.book2.chapter, verse: verse.verse! },
+            background_color: highlightBackgroundColor
+          }));
+
+          // TODO - saves the last highlighted verse not the present
+          // TODO - can't change already highlighted color
+          setBook({
+            ...book,
+            book2: {
+              ...book.book2,
+              highlightedVerses: [
+                ...book.book2.highlightedVerses,
+                ...newHighlights
+              ]
+            }
+          });
+        }
 
         setIsVerseFocused([])
       } else {
@@ -82,47 +92,88 @@ const VersePopover = ({
           });
         }
 
-        console.log(book.book1.highlightedVerses)
-
         setIsVerseFocused([])
       }
     }
   };
+
+  // TODO - finish this function
+  // function removeHighlight() { }
 
   return (
     <Popover>
       <PopoverTrigger asChild>
         {children}
       </PopoverTrigger>
-      <PopoverContent className="w-52 p-2 flex flex-col gap-1">
-        <div className="flex flex-col gap-2">
-          <Label className="text-muted-foreground text-xs text-center">Highlight</Label>
-          <div className="flex flex-row justify-between">
-            <span
-              className="w-[30px] h-[30px] rounded-full cursor-pointer flex items-center justify-center border"
-              onClick={() => handleHighlightVerse("")}
+      <PopoverContent className="w-52 p-1 flex gap-3 items-center shadow-md shadow-zinc-300 dark:shadow-zinc-800">
+        <div className="flex items-center">
+          <Button
+            variant="ghost"
+            className="h-8 w-8 p-0 rounded-sm"
+          >
+            <Bold />
+          </Button>
+          <Button
+            variant="ghost"
+            className="h-8 w-8 p-0 rounded-sm"
+          >
+            <Italic />
+          </Button>
+          <Button
+            variant="ghost"
+            className="h-8 w-8 p-0 rounded-sm"
+          >
+            <Bookmark />
+          </Button>
+          <div className="flex">
+            <Button
+              variant="ghost"
+              className="h-8 w-8 p-0 rounded-sm"
+              onClick={() => handleHighlightVerse(currentHighlightColor)}
             >
-              <X />
-            </span>
-            <span
-              className="w-[30px] h-[30px] bg-green-300 rounded-full cursor-pointer"
-              onClick={() => handleHighlightVerse("#86efac")}
-            ></span>
-            <span
-              className="w-[30px] h-[30px] bg-blue-300 rounded-full cursor-pointer"
-              onClick={() => handleHighlightVerse("#93c5fd")}
-            ></span>
-            <span
-              className="w-[30px] h-[30px] bg-red-300 rounded-full cursor-pointer"
-              onClick={() => handleHighlightVerse("#fca5a5")}
-            ></span>
-            <span
-              className="w-[30px] h-[30px] bg-yellow-300 rounded-full cursor-pointer"
-              onClick={() => handleHighlightVerse("#fde047")}
-            ></span>
+              <Highlighter />
+            </Button>
+            <DropdownMenu modal={false}>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="ghost"
+                  className="h-8 w-4 p-0 rounded-sm"
+                >
+                  <ChevronDown />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-[45px] min-w-0 flex flex-col gap-1">
+                <DropdownMenuItem
+                  className="p-0 border h-[30px] cursor-pointer focus:bg-zinc-200 dark:focus:bg-zinc-800 flex items-center justify-center"
+                  onClick={() => handleHighlightVerse("")}
+                >
+                  <X />
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className="p-0 bg-green-300 h-[30px] cursor-pointer focus:bg-green-400"
+                  onClick={() => handleHighlightVerse("#86efac")}
+                >
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className="h-[30px] bg-blue-300 cursor-pointer focus:bg-blue-400"
+                  onClick={() => handleHighlightVerse("#93c5fd")}
+                >
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className="h-[30px] bg-red-300 cursor-pointer focus:bg-red-400"
+                  onClick={() => handleHighlightVerse("#fca5a5")}
+                >
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  className="h-[30px] bg-yellow-300 cursor-pointer focus:bg-yellow-400"
+                  onClick={() => handleHighlightVerse("#fde047")}
+                >
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
-        <Separator />
+        <Separator orientation="vertical" className="h-8 w-[1.5px]" />
         <div className="flex flex-row justify-between gap-2">
           <CopyToClipBoardButton verse={verse} book={book} book2={book2} />
         </div>
